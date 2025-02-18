359e92c5cec562ade8e6896470465f053dbe87cf
mony120 <ananasedit@gmail.com>
2025-02-18 12:38:04 +0900

    ура, рецепты в бд!!! уху

---
+++ app/src/main/java/com/example/apprecipe/Recipe.kt
@@ -0,0 +1,7 @@
+package com.example.apprecipe
+
+data class Recipe(
+    val name: String = "",
+    val time: String = "",
+    val url: String = ""
+)
--- app/src/main/java/com/example/apprecipe/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/apprecipe/ui/notifications/NotificationsFragment.kt
@@ -1,35 +1,63 @@
 package com.example.apprecipe.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apprecipe.Recipe
+import com.example.apprecipe.RecipeAdapter
 import com.example.apprecipe.databinding.FragmentNotificationsBinding
+import com.google.firebase.database.*
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recipeAdapter: RecipeAdapter
+    private val recipeList = mutableListOf<Recipe>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        recyclerView = binding.recycleView // Убедитесь, что у вас есть RecyclerView в вашем XML
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recipeAdapter = RecipeAdapter(recipeList)
+        recyclerView.adapter = recipeAdapter
+
+        fetchRecipesFromFirebase()
 
         return root
+    }
+
+    private fun fetchRecipesFromFirebase() {
+        val recipesRef = FirebaseDatabase.getInstance().getReference("recipes")
+
+        recipesRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                recipeList.clear()
+                for (recipeSnapshot in dataSnapshot.children) {
+                    val recipe = recipeSnapshot.getValue(Recipe::class.java)
+                    if (recipe != null) {
+                        recipeList.add(recipe)
+                    }
+                }
+                recipeAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("NotificationFragment", "Load Database Error: ${databaseError.message}")
+            }
+        })
     }
 
     override fun onDestroyView() {
