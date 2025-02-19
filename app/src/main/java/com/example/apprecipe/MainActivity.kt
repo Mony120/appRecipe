8cace6da850538150457c1a49636ddcbdf7b01d2
mony120 <ananasedit@gmail.com>
2025-02-19 19:26:50 +0900

    полностью рабочая регистрация, все супер без багов с ней

--- app/src/main/java/com/example/apprecipe/MainActivity.kt
+++ app/src/main/java/com/example/apprecipe/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -31,15 +32,35 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.setupWithNavController(navController)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_setting
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        setupBottomNavigation(navController)
+    }
+    private fun setupBottomNavigation(navController: NavController) {
+        binding.navView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home)
+                    true
+                }
+                R.id.navigation_dashboard -> {
+                    navController.navigate(R.id.navigation_dashboard)
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    navController.navigate(R.id.navigation_notifications)
+                    true
+                }
+                R.id.navigation_setting -> {
+                    navController.navigate(R.id.navigation_setting)
+                    true
+                }
+                // Добавьте дополнительные элементы меню по мере необходимости
+                else -> false
+            }
+        }
     }
     fun hideBottomNavigation() {
         binding.navView.visibility = View.GONE
--- app/src/main/java/com/example/apprecipe/ui/settings/SettingFragment.kt
+++ app/src/main/java/com/example/apprecipe/ui/settings/SettingFragment.kt
@@ -2,16 +2,31 @@ package com.example.apprecipe.ui.settings
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.apprecipe.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class SettingFragment : Fragment() {
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var myRef: DatabaseReference
     private lateinit var switch: Switch
 
     override fun onCreateView(
@@ -27,10 +42,7 @@ class SettingFragment : Fragment() {
         // Восстановление состояния Switch из SharedPreferences
         val sharedPreferences = requireContext().getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
         val savedTheme = sharedPreferences.getInt("ThemeMode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-        when (savedTheme) {
-            AppCompatDelegate.MODE_NIGHT_YES -> switch.isChecked = true
-            AppCompatDelegate.MODE_NIGHT_NO -> switch.isChecked = false
-        }
+        switch.isChecked = savedTheme == AppCompatDelegate.MODE_NIGHT_YES
 
         // Обработчик переключения Switch
         switch.setOnCheckedChangeListener { _, isChecked ->
@@ -41,9 +53,66 @@ class SettingFragment : Fragment() {
             }
             AppCompatDelegate.setDefaultNightMode(newThemeMode)
             sharedPreferences.edit().putInt("ThemeMode", newThemeMode).apply()
-            activity?.recreate() // Перезапуск активности для применения темы
+            // Обновление UI без перезапуска активности
         }
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        myRef = database.getReference("users")
+
+        val emailTextView: TextView = view.findViewById(R.id.tv_email)
+        val loginTextView: TextView = view.findViewById(R.id.tv_name)
+        val btnLogout: Button = view.findViewById(R.id.btn_logout)
+
+        val user = auth.currentUser
+
+        if (user != null) {
+            if (user.isEmailVerified) { // Проверка, подтвержден ли email
+                emailTextView.text = "Email: ${user.email}"
+                getUserName(user.uid, loginTextView)
+            } else {
+                // Если email не подтвержден, перенаправляем на экран входа
+                Log.d("SettingFragment", "Email не подтвержден")
+                Toast.makeText(context, "Пожалуйста, подтвердите свой адрес электронной почты.", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.navigation_login)
+            }
+        } else {
+            // Если пользователь не зарегистрирован, перенаправляем на экран входа
+            findNavController().navigate(R.id.navigation_login)
+        }
+
+        btnLogout.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun getUserName(userId: String, loginTextView: TextView) {
+        lifecycleScope.launch {
+            try {
+                val name = withContext(Dispatchers.IO) {
+                    val dataSnapshot = myRef.child(userId).get().await()
+                    if (dataSnapshot.exists()) {
+                        dataSnapshot.child("username").getValue(String::class.java)
+                    } else {
+                        Log.d("SettingFragment", "Пользователь не найден в базе данных")
+                        null
+                    }
+                }
+                loginTextView.text = "Имя: ${name ?: "Неизвестно"}"
+            } catch (e: Exception) {
+                Log.w("SettingFragment", "Ошибка при получении данных", e)
+            }
+        }
+    }
+
+    private fun logOut() {
+        auth.signOut()
+        findNavController().navigate(R.id.navigation_login)
     }
 }
